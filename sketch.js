8f4890e36b4273dd1e76cfacd774cbcbbf6eeda4
DDDDDYLAN5050 <55461461+DDDDDYLAN5050@users.noreply.github.com>
2020-06-05 21:44:59 +0200

    init

--- sketch.js
+++ sketch.js
@@ -1,11 +1,44 @@
-function preload(){
-  // put preload code here
+let dialog, dialogBox;
+let choice, choiceBox;
+var numLine;
+var n = 0;
+var url_string = window.location.pathname;
+
+function preload() {
+  // console.log(url_string);
+  var chapter = url_string.replace("/", "");
+  chapter = chapter.replace(".html", "");
+  console.log(chapter);
+  dialog = loadTable("./assets/" + chapter + "dialog.csv", 'csv');
+  choice = loadTable("./assets/" + chapter + "choice.csv", 'csv');
+
 }
 
 function setup() {
-  // put setup code here
+  noCanvas();
+  numLine = dialog.getRowCount();
+  print(numLine + ' total rows in table');
+  dialogBox = select(".dialogBox");
+  dialogBox.mouseClicked(dialogClicked);
+  choiceBox = select(".choiceBox");
+  // choiceBox.hide();
 }
 
-function draw() {
-  // put drawing code here
+function dialogClicked() {
+  if (n < numLine) {
+    print(n);
+    document.getElementById("dialog").innerHTML = dialog.getString(n, 1);
+    document.getElementById("character").innerHTML = dialog.getString(n, 0);
+    n++;
+  } else if (n == numLine) {
+    print("make choice");
+    choiceBox.show();
+    for (i = 1; i < choice.getRowCount(); i++) {
+      var value = choice.getString(i, 1);
+      var jump2where = choice.getString(i, 2);
+      var button = '</br><input type="button" class="buttonChoice" value="' + value + '" onclick="window.location.href = ' + "'" + './' + jump2where + '.html' + "'" + '"/>';
+      select(".choiceBox").html(button, true);
+    }
+    n++;
+  }
 }
